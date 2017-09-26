4ef744f8c333477e2a6d4113cfdfa9c820ed0b99
Abi Travers <atravers4007@gmail.com>
2017-09-26 14:57:48 +0100

    Character added back

--- views/logic/drawingcharacter.js
+++ views/logic/drawingcharacter.js
@@ -1,21 +1,11 @@
-
-var Drawcircle = function (xCoord){
-
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+ctx.beginPath(0, 400);
 ctx.fillStyle = ("black");
 ctx.beginPath();
-ctx.arc(xCoord,370,20,0,2*Math.PI);
+ctx.arc(50,360,20,0,2*Math.PI);
+ctx.fillStyle = "black";
+ctx.stroke();
+
 ctx.closePath();
 ctx.fill();
-};
-// )};
-
-Drawcircle.Prototype.moveXright = function () {
-  xCoordinate += 10;
-}
-
-drawcircle = new Drawcircle
-var xCoordinate = 60
-drawcircle(xCoordinate)
-document.onkeydown = drawcircle.moveXright;
