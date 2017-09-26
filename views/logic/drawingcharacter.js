c0f90abf690d4c50107b702bb19c0cdbea5b76ad
Abi Travers <atravers4007@gmail.com>
2017-09-26 12:17:12 +0100

    Character added

--- views/logic/drawingcharacter.js
+++ views/logic/drawingcharacter.js
@@ -1,8 +1,7 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-ctx.beginPath(0, 400);
+ctx.fillStyle = ("black");
+ctx.beginPath();
 ctx.arc(50,360,20,0,2*Math.PI);
-ctx.fillStyle = "black";
-ctx.stroke();
-
-;
+ctx.closePath();
+ctx.fill();
