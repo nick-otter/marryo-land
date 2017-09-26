e4d6a3e7771bf53e65f7e1235cc017fd26bd3231
Abi Travers <atravers4007@gmail.com>
2017-09-26 12:56:48 +0100

    Moving character

--- views/logic/drawingcharacter.js
+++ views/logic/drawingcharacter.js
@@ -1,7 +1,19 @@
+
+var drawcircle = function (xCoord){
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.fillStyle = ("black");
 ctx.beginPath();
-ctx.arc(50,360,20,0,2*Math.PI);
+ctx.arc(xCoord,370,20,0,2*Math.PI);
 ctx.closePath();
 ctx.fill();
+};
+// )};
+
+var xCoordinate = 60
+
+drawcircle(xCoordinate)
+
+drawcircle.Prototype.moveXright = function () {
+  xCoordinate += 10;
+}
