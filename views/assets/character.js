06abe5b0e48f5c4b97d2dae76221def611bc5dc8
Abi Travers <atravers4007@gmail.com>
2017-09-27 16:18:31 +0100

    integration of jump and testing updates

--- views/assets/character.js
+++ views/assets/character.js
@@ -32,6 +32,9 @@ console.log(e);
 
 this.keyCode = e.keyCode;
 
+if(this.keyCode!==38){
+
+
  if(this.keyCode==39){
    xPos+=5;
  }
@@ -39,14 +42,28 @@ this.keyCode = e.keyCode;
  if(this.keyCode==37){
    xPos-=5;
  }
- if(this.keyCode==38){
-   yPos-=5;
- }
 
  if(this.keyCode==40){
-   yPos+=5;
+     yPos+=5;
+   }
+
+   moveandrender();
+ };
+
+
+ if(this.keyCode==38){
+   for(var i = 0; i<10; i++){
+     jumpMotion(i,30,10,false)
+     jumpMotion(9-i,30,9,true);
+   }
  }
+}
 
- moveandrender();
-
+var jumpMotion = function(amtToMove, stepDist, numOfUpSteps, isDown){
+  var delay = 0, upOrDown=1
+  if(isDown) { delay = stepDist*numOfUpSteps+70; upOrDown = -1 }
+  setTimeout(function(){
+    yPos -= 4 * amtToMove * upOrDown;
+    moveandrender();
+  },(numOfUpSteps - amtToMove) * stepDist + delay)
 };
