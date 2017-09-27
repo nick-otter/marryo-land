921d4cf136633369815bc96a5870d088d71bea3d
Nick Otter <nicholas.a.otter@gmail.com>
2017-09-27 12:54:25 +0100

    Added: testrunner and character test

---
+++ tests/unitTests/character_spec.js
@@ -0,0 +1,25 @@
+beforeEach(function() {
+  var canvas = document.querySelector('#myCanvas');
+  var context = canvas.getContext('2d');
+  var xPos = 30;
+  var yPos = 370;
+});
+
+character = new Character;
+
+describe('Character', function() {
+
+//   describe('#init', function() {
+//     it('it draws the character when instantiated', function(){
+//       expect(character).to.be.instanceof(Character);
+//     });
+//   });
+
+  describe('#move', function() {
+    it('it moves the character left', function(){
+      character.move.keyCode = 39;
+      assert.equal(xPos, 35);
+    });
+  });
+
+});
