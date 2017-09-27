badd86d17777b8528ebfc76f95320f9e8d197142
Abi Travers <atravers4007@gmail.com>
2017-09-27 16:28:47 +0100

    Deleted test put back in

--- tests/unitTests/character_spec.js
+++ tests/unitTests/character_spec.js
@@ -1,1 +1,25 @@
-"Hello World"
+beforeEach(function() {
+  var character = new Character;
+  var canvas = document.querySelector('#myCanvas');
+  var context = canvas.getContext('2d');
+  var xPos = 30;
+  var yPos = 370;
+});
+
+
+describe('Character', function() {
+
+//   describe('#init', function() {
+//     it('it draws the character when instantiated', function(){
+//       expect(character).to.be.instanceof(Character);
+//     });
+//   });
+
+  describe('#move', function() {
+    it('it moves the character left', function(){
+      character.move.keyCode = 39;
+      assert.equal(xPos, 35);
+    });
+  });
+
+});
