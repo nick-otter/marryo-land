c8e6dfeaa63c4cd624b225b7d4c8b469008af578
Abi Travers <atravers4007@gmail.com>
2017-09-28 11:22:45 +0100

    refactor and coin

--- views/assets/character.js
+++ views/assets/character.js
@@ -1,13 +1,12 @@
 var xPos = 30;
 var yPos = 370;
 
+
 function Character (){
 this.jumping = false
 // var c = document.getElementById("myCanvas");
 // var ctx = c.getContext("2d");
-
 // Sets location
-
 context.beginPath(0, 400);
 context.fillStyle = ("black");
 context.beginPath();
@@ -16,22 +15,15 @@ context.fillStyle = "black";
 context.stroke();
 context.closePath();
 context.fill();
-
 // context.rect(xPos, yPos, 50, 50);
 // context.stroke();
-
 };
 
 Character.prototype.move= function (e){
-
 // To find keyCode uncomment below:
 // alert(e.keyCode)
-
 this.keyCode = e.keyCode;
-
 if(this.keyCode!==38){
-
-
  if(this.keyCode==39){
    xPos+=5;
  }
@@ -39,24 +31,18 @@ if(this.keyCode!==38){
  if(this.keyCode==37){
    xPos-=5;
  }
-
  if(this.keyCode==40){
      yPos+=5;
    }
-
    moveandrender();
  };
-
-
  if(this.keyCode==38){
    for(var i = 0; i<10; i++){
      _jumpMotion(i,30,10,false)
      _jumpMotion(9-i,30,9,true);
    }
  }
-
 }
-
 var _jumpMotion = function(amtToMove, stepDist, numOfUpSteps, isDown){
   var delay = 0, upOrDown=1
   if(isDown) { delay = stepDist*numOfUpSteps+70; upOrDown = -1 }
@@ -64,4 +50,4 @@ var _jumpMotion = function(amtToMove, stepDist, numOfUpSteps, isDown){
     yPos -= 4 * amtToMove * upOrDown;
     moveandrender();
   },(numOfUpSteps - amtToMove) * stepDist + delay)
-};
+}
--- views/assets/coin.js
+++ views/assets/coin.js
@@ -1,16 +1,11 @@
-// function coin (){
-//
-// var c = document.getElementById("myCanvas");
-// var ctx = c.getContext("2d");
-//
-// // Sets location
-// context.beginPath(0, 400);
-// context.fillStyle = ("black");
-// context.beginPath();
-// context.arc(90,570,20,0,2*Math.PI);
-// context.fillStyle = "black";
-// context.stroke();
-// context.closePath();
-// context.fill();
-//
-// };
+
+function coin (){
+  context.beginPath(0, 400);
+  context.fillStyle = ("white");
+  context.beginPath();
+  context.arc(200,100,20,0,2*Math.PI);
+  context.fillStyle = "white";
+  context.stroke();
+  context.closePath();
+  context.fill();
+};
