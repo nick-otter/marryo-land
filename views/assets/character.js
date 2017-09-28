b2fc6dd64f46834dff01be1c03274739a89f430f
Will Porter <willjsporter@hotmail.co.uk>
2017-09-28 14:50:56 +0100

    jumping working again following group discussion after refactor

--- views/assets/character.js
+++ views/assets/character.js
@@ -7,33 +7,34 @@ this.yPos = 370;
 
 Character.prototype.moveLeft = function(){
    this.xPos-=5;
-   console.log(this.xPos)
+   console.log(this)
  };
 
 
  Character.prototype.moveRight = function(){
     this.xPos+=5;
-    console.log(this.xPos)
+    console.log(this)
   };
 
   Character.prototype.moveDown = function(){
      this.yPos+=5;
-     console.log(this.xPos)
+     console.log(this)
    };
 
   Character.prototype.jump = function(){
     for(var i = 0; i<10; i++){
-      _jumpMotion(i,30,10,false)
-      _jumpMotion(9-i,30,9,true);
+      this._jumpMotion(i,30,10,false)
+      this._jumpMotion(9-i,30,9,true);
     }
-     console.log(this.yPos)
+     console.log(this)
    };
 
 
- function _jumpMotion(amtToMove, stepDist, numOfUpSteps, isDown){
-   var delay = 0, upOrDown=1
+ Character.prototype._jumpMotion = function (amtToMove, stepDist, numOfUpSteps, isDown){
+   var delay = 0, upOrDown=1, self = this;
    if(isDown) { delay = stepDist*numOfUpSteps+70; upOrDown = -1 }
    setTimeout(function(){
+     console.log(this)
      this.yPos -= 4 * amtToMove * upOrDown;
    },(numOfUpSteps - amtToMove) * stepDist + delay)
  };
