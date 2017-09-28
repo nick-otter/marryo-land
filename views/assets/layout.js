18c1b4e2d994fad9e4224df6cd96ad66be09420f
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-09-28 12:22:43 +0100

    separation of concerns

--- views/assets/layout.js
+++ views/assets/layout.js
@@ -2,18 +2,45 @@
 var canvas = document.querySelector('#myCanvas');
 var context = canvas.getContext('2d');
 
-// Sets location
-var xPos = 30;
-var yPos = 370;
+// makes coin
+function coin (){
+  context.beginPath(0, 400);
+  context.fillStyle = ("white");
+  context.beginPath();
+  context.arc(200,100,20,0,2*Math.PI);
+  context.fillStyle = "white";
+  context.stroke();
+  context.closePath();
+  context.fill();
+};
 
-// Renders circle and line assets
-var mario = new Character
-line();
+// var mario = new Character
+
+
+function drawCharacter(){
+context.beginPath(0, 400);
+context.fillStyle = ("black");
+context.beginPath();
+context.arc(mario.xPos, mario.yPos,20,0,2*Math.PI);
+context.fillStyle = "black";
+context.stroke();
+context.closePath();
+context.fill();
+};
+
+
+function line (){
+
+context.beginPath();
+context.moveTo(0, 400);
+context.lineTo(900, 400);
+context.stroke();
+};
+
 
 function moveandrender (){
-
   canvas.width=canvas.width;
-  var mario = new Character;
   line();
-
+  coin();
+  drawCharacter();
 };
--- views/assets/line.js
+++
@@ -1,7 +0,0 @@
-function line (){
-
-context.beginPath();
-context.moveTo(0, 400);
-context.lineTo(900, 400);
-context.stroke();
-};
